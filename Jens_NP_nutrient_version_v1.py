0a0043f22205d954b35ab440bfdae4ae1f2f3acd
JensMNielsen <jens.nielsen@noaa.gov>
2024-11-18 15:26:55 -0800

    jens_version_maybe

---
+++ Jens_NP_nutrient_version_v1.py
@@ -0,0 +1,10 @@
+import numpy as np
+import scipy
+import scipy.integrate
+import matplotlib.pyplot as plt
+import pandas as pd
+import sympy as sp
+
+# data input 
+m2 = pd.read_csv('jens_data/full_2018_data_M2_Bering_combined_data.csv')
+
